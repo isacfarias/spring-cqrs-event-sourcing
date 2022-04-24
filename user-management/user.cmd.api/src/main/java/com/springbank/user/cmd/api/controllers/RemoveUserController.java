a6c969962a2f6b7b00b13bd6becd8b470548e017
isacfarias <isac.farias.21@gmail.com>
2022-04-24 15:30:51 -0400

    add controller remove

---
+++ user-management/user.cmd.api/src/main/java/com/springbank/user/cmd/api/controllers/RemoveUserController.java
@@ -0,0 +1,40 @@
+package com.springbank.user.cmd.api.controllers;
+
+import com.springbank.user.cmd.api.commands.RemoveUserCommand;
+import com.springbank.user.cmd.api.dto.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/v1/removeUser")
+public class RemoveUserController {
+
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public RemoveUserController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @DeleteMapping(path = "/{id]")
+    public ResponseEntity<BaseResponse> removeUser(@PathVariable(value = "id") String id) {
+        try {
+            commandGateway.send(new RemoveUserCommand(id));
+            return new ResponseEntity<>(new BaseResponse("User successfully removed"), HttpStatus.OK);
+        } catch (Exception e) {
+            var safeError = "Error while processing remove user request for id - ".concat(id);
+            log.error(safeError);
+            return new ResponseEntity<>(new BaseResponse(safeError), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+}
