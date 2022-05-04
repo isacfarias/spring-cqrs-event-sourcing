5c4c7f464146dd8c5aa7e3192ebc0d21b3a8409e
isacfarias <isac.farias.21@gmail.com>
2022-05-03 21:26:40 -0400

    add model para bankAccount

---
+++ bank-account/bankacc.core/src/main/java/com/springbank/bankacc/core/models/BankAccount.java
@@ -0,0 +1,45 @@
+package com.springbank.bankacc.core.models;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class BankAccount {
+
+    @Id
+    private String id;
+    private String accountHolderId;
+    private Date creationDate;
+    private AccountType accountType;
+    private double balance;
+
+
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        BankAccount that = (BankAccount) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
